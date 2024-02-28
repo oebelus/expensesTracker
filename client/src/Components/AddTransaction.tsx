1a25c85d5524757143418545063788a4f2c4621b
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-02-28 13:53:14 +0100

    Add Transactions Table

--- client/src/Components/AddTransaction.tsx
+++ client/src/Components/AddTransaction.tsx
@@ -2,19 +2,26 @@ import axios from "axios"
 import { useState } from "react"
 import { getError } from "../../utils"
 import { ApiError } from "../types/ApiError"
+import { Transaction } from "../types/Transaction"
 
 export default function Wallet() {
-  const [name, setName] = useState("")
-  const [amount, setAmount] = useState("")
-  const [category, setCategory] = useState("")
-  const [date, setDate] = useState("")
-  const [recurring, setRecurring] = useState(false)
+  const [name, setName] = useState<Transaction['name']>("")
+  const [amount, setAmount] = useState<Transaction['amount']>("")
+  const [category, setCategory] = useState<Transaction['category']>("")
+  const [date, setDate] = useState<Transaction['date']>(new Date())
+  const [recurring, setRecurring] = useState<Transaction['recurring']>(false)
 
   const userId = localStorage.getItem("userId")
 
   const dictionary: Record<string, boolean> = {
     "yes": true,
     "no": false
+  }
+
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedDate = e.target.value
+    const parseDate = new Date(selectedDate)
+    setDate(parseDate)
   }
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
@@ -41,28 +48,32 @@ export default function Wallet() {
           </div>
           <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
             <div className="col-span-full sm:col-span-3">
-              <label htmlFor="firstname" className="text-sm">Name</label>
+              <label htmlFor="firstname" className="text-sm mb-2">Name</label>
               <input value={name} onChange={(e) => setName(e.target.value)} id="name" type="text" placeholder="Name" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" />
             </div>
             <div className="col-span-full sm:col-span-3">
-              <label htmlFor="transaction" className="text-sm">Amount</label>
+              <label htmlFor="transaction" className="text-sm mb-2">Amount</label>
               <input value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" type="number" placeholder="Amount" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" required/>
             </div>
             <div className="col-span-full sm:col-span-3">
-              <label htmlFor="category" className="text-sm">Category</label>
+              <label htmlFor="category" className="text-sm mb-2">Category</label>
               <input value={category} onChange={(e) => setCategory(e.target.value)} id="category" type="text" placeholder="Category" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" required/>
             </div>
             <div className="col-span-full sm:col-span-3">
-              <label htmlFor="date" className="text-sm">Date</label>
-              <input value={date} onChange={(e) => setDate(e.target.value)} id="date" type="date" placeholder="Date" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" required/>
+              <label htmlFor="date" className="text-sm mb-2">Date</label>
+              <input value={date.toLocaleDateString()} onChange={handleDateChange} id="date" type="date" placeholder="Date" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" required/>
             </div>
             <div className="col-span-full sm:col-span-2">
-              <label htmlFor="state" className="text-sm block">Is it recurring?</label>
-                <div className="flex items-center">
-                  <input onChange={(e) => setRecurring(dictionary[e.target.value])} id="yes" type="radio" name="recurring" value="yes" className="mr-1 h-4 w-4 text-blue-600 focus:ring-blue-500" required/>
-                  <label htmlFor="yes" className="text-sm mr-4 cursor-pointer select-none">Yes</label>
-                  <input onChange={(e) => setRecurring(dictionary[e.target.value])} id="no" type="radio" name="recurring" value="no" className="mr-1 h-4 w-4 text-blue-600 focus:ring-blue-500"required />
-                  <label htmlFor="no" className="text-sm cursor-pointer select-none">No</label>
+              <label htmlFor="state" className="text-sm block mb-2">Is it recurring?</label>
+                <div className="flex">
+                  <div className="flex items-center mr-4">
+                      <input id="default-radio-1" type="radio" onChange={(e) => setRecurring(dictionary[e.target.value])} value="yes" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                      <label htmlFor="default-radio-1" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Yes</label>
+                  </div>
+                  <div className="flex items-center">
+                      <input checked id="default-radio-2" type="radio" onChange={(e) => setRecurring(dictionary[e.target.value])} value="no" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                      <label htmlFor="default-radio-2" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">No</label>
+                  </div>
                 </div>
             </div>
           </div>
--- client/src/Components/Transactions.tsx
+++ client/src/Components/Transactions.tsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Transaction } from "../types/Transaction";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoneyBillAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function Transactions() {
     const [data, setData] = useState([]);
@@ -15,44 +13,92 @@ export default function Transactions() {
             });
     }, [userId]); // Add userId to the dependency array
 
+    function day(date: Date): string {
+        const days: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        return days[date.getDay()];
+    }   
+    
+    function format(date: Date): string {
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const arr = date.toDateString().split(" ").splice(1, 4);
+        const day = arr[1];
+        arr[0] = day;
+        arr[1] = months[date.getMonth()];
+        return arr.join(" ");
+    }
+
     return (
         <div>
-            <div className="mt-6 md:flex md:items-center md:justify-between">
-                <div className="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
-                    <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300">
-                        View all
-                    </button>
+            <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100">
+                <h2 className="mb-4 text-2xl font-semibold leadi">My Transactions</h2>
+                <div className="mt-6 md:flex md:items-center md:justify-between mb-6">
+                    <div className="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
+                        <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300">
+                            View all
+                        </button>
 
-                    <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
-                        Category
-                    </button>
+                        <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+                            Category 
+                        </button>
 
-                    <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
-                        Price
-                    </button>
+                        <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+                            Price
+                        </button>
+                    </div>
+
+                    <div className="relative flex items-center mt-4 md:mt-0">
+                        <span className="absolute">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                            </svg>
+                        </span>
+
+                        <input type="text" placeholder="Search" className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                    </div>
                 </div>
-
-                <div className="relative flex items-center mt-4 md:mt-0">
-                    <span className="absolute">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                        </svg>
-                    </span>
-
-                    <input type="text" placeholder="Search" className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                <div className="overflow-x-auto mt-6">
+                    <table className="min-w-full text-xs">
+                        <colgroup>
+                            <col />
+                            <col />
+                            <col />
+                            <col />
+                            <col />
+                        </colgroup>
+                        <thead className="dark:bg-gray-700">
+                            <tr className="text-left">
+                                <th className="p-3 text-center">Date</th>
+                                <th className="p-3 text-center">Name</th>
+                                <th className="p-3 text-center">Amount</th>
+                                <th className="p-3 text-center">Category</th>
+                                <th className="p-3 text-center">Recurrence</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {data.map((el: Transaction, key: number) => (
+                            <tr key={key} className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+                                <td className="p-3">
+                                    <p className="text-center">{format(new Date(el.date))}</p>
+                                    <p className="dark:text-gray-400 text-center">{day(new Date(el.date))}</p>
+                                </td>
+                                <td className="p-3">
+                                    <p className="text-center">{el.name}</p>
+                                </td>
+                                <td className="p-3">
+                                    <p className="text-center">{el.amount}</p>
+                                </td>
+                                <td className="p-3">
+                                    <p className="text-center">{el.category}</p>
+                                </td>
+                                <td className="p-3 text-right">
+                                    <p className="text-center">{el.recurring.toString()}</p>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
-            {data.map((el: Transaction, key: number) => (
-                <div key={key} className="flex items-center p-4 space-x-4 rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-100">
-                    <div className="flex items-center self-stretch justify-center bg-gray-300 dark:bg-gray-700 rounded-full p-2">
-                       <FontAwesomeIcon icon={faMoneyBillAlt} className="p-2"/>
-                    </div>
-                    <div className="flex flex-col">
-                        <span className="font-semibold">{el.name}: {el.amount}$</span>
-                        <span className="text-sm">{el.category}</span>
-                    </div>
-                </div>
-            ))}
         </div>
     );
 }
