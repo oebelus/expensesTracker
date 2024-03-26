c6e1e2e1a389f6ace6bf96383df0aefdd3ee4f67
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-03-26 14:27:51 +0000

    update server

--- server/src/controller/session.controller.ts
+++ server/src/controller/session.controller.ts
@@ -1,6 +1,7 @@
 import { generateToken, getGoogleOAuthTokens, getGoogleUser } from "../utils"
 import { Request, Response } from "express"
 import { UserModel } from "../models/userModel"
+import bcrypt from 'bcrypt'
 
 export async function googleOauthHandler(req: Request, res: Response) {
     try {
@@ -12,38 +13,38 @@ export async function googleOauthHandler(req: Request, res: Response) {
 
         // Get the user with tokens
         const googleUser = await getGoogleUser({id_token, access_token})
+        console.log("Google user: ", googleUser)
         /*jwt.decode(id_token)*/
 
         if (!googleUser.verified_email) {
-            return res.status(403).send('Google account is not verified')
+            return res.status(403).json({error: 'Google account is not verified'})
         }
 
         // Upsert the user
-        const user = await UserModel.findOneAndUpdate(
-            {
-                email: googleUser.email,
-            }, {
+        const salt = bcrypt.genSaltSync(10);
+        let user = await UserModel.findOne({ email: googleUser.email });
+        if (user) {
+            res.cookie("userId", user!._id)
+            res.cookie("userInfo", user)
+        } else {
+            user = await UserModel.create({
                 email: googleUser.email,
                 firstName: googleUser.given_name,
                 familyName: googleUser.family_name,
-                image: googleUser.picture
-            }, {
-                upsert: true,
-                new: true
-            }
-        )
-        console.log(user)
+                password: bcrypt.hashSync("123456789", salt),
+                image: googleUser.picture,
+            });
+        
+            // Create access & refresh tokens
+            const token = generateToken(user!)
 
-        // Create access & refresh tokens
-        const token = generateToken(user!)
-
-        // Set cookies
-        res.cookie("token", token)
-        res.cookie("userId", user!._id)
-        res.cookie("userInfo", user)
-
+            // Set cookies
+            res.cookie("token", token)
+            res.cookie("userId", user!._id)
+            res.cookie("userInfo", user)
+        }
         // Redirect back to client
-        res.redirect(`http://localhost:5173/${user!._id}/dashboard`)
+        res.redirect(`http://localhost:5173/`)
 
     } catch (error) {
         console.error(error, "Failed to authorize Google user")
